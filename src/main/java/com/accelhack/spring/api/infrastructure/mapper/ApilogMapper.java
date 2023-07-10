8b86fabc0185459fe655660f61ba963848d49d2d
coffeemountain <coffee.mountain551@gmail.com>
2023-07-10 11:23:55 +0900

    テスト通過するように修正

--- src/main/java/com/accelhack/spring/api/infrastructure/mapper/ApilogMapper.java
+++ src/main/java/com/accelhack/spring/api/infrastructure/mapper/ApilogMapper.java
@@ -1,9 +1,7 @@
 package com.accelhack.spring.api.infrastructure.mapper;
 
 import java.util.UUID;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import com.accelhack.spring.api.infrastructure.dto.ApilogDto;
 
 @Mapper
--- src/test/java/com/accelhack/spring/api/ApiApplicationTests.java
+++ src/test/java/com/accelhack/spring/api/ApiApplicationTests.java
@@ -1,12 +1,21 @@
 package com.accelhack.spring.api;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("l-test")
 @SpringBootTest(classes = ApiApplication.class)
 public class ApiApplicationTests {
+
+  @Autowired
+  private Environment env;
+
   @Test
-  public void contextLoads() {}
+  public void contextLoads() {
+    System.out.println("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+  }
 }
